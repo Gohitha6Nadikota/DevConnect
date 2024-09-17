ab0872f907e6d18afeee9a6a9416cb66a9bdb546
Gohitha6Nadikota <gohithapriyanadikota@gmail.com>
2024-09-17 22:43:28 +0530

    Handling routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 const express=require('express');
 
 const app=express();
-app.use("/",(req,res)=>{
-    res.send("Hello from Server");
+app.get('/user/:userId',(req,res)=>{
+  console.log(req.params)
+  res.send("Data Sent");
 })
-app.use("/home", (req, res) => {
-  res.send("Hello from Home Page");
-});
+app.post('/user',(req,res)=>{
+  console.log(req.query)
+  res.send("Data Saved");
+})
+app.delete('/user',(req,res)=>{
+  res.send("Data deleted");
+})
+app.patch('/user',(req,res)=>{
+  res.send("Data patched")
+})
+app.put('/user',(req,res)=>{
+  res.send("Data modified")
+})
 app.listen(7777,()=>{
     console.log("Server Started");
 });
